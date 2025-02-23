f715029132dbed4873c7a6093e55d3e7345a649d
Munity <38476677+MunityVR@users.noreply.github.com>
2025-02-23 09:39:19 -0500

    Simplified while loop

--- Dice.py
+++ Dice.py
@@ -1,14 +1,10 @@
 import random as r
 
-# This keeps it looping.
-a = 0.1
-b = 0.2
-
 # Setting random integer as dice (Number Cube).
 dice = r.randint
 
 # Whileloops dice (r.randint), and ask "Do you wanna exit", y = yes, n = no.
-while a != b:
+while true:
     print("You rolled : " + str(dice(1,6)))
     ab = str(input("Do you wanna exit y/n : "))
     if ab == 'y' or ab == 'Y':
